5c5a28f13dfb94ab45358a6e1b7d98ae8cc5e3f1
YonasWorkneh <yonasdegefu158@gmail.com>
2026-05-08 15:51:38 +0300

    fix: update API base URL reference in auth module to use VITE_API_BASE_URL

--- src/lib/api/auth.ts
+++ src/lib/api/auth.ts
@@ -3,7 +3,7 @@ import type { LoginResponse, RegisterResponse } from "@/types/auth";
 // const BASE_URL = import.meta.env.VITE_BASE_URL;
 // const BASE_URL = "https://test-courier.servehalflife.com";
 // const BASE_URL = "http://localhost:10000";
-const BASE_URL = import.meta.env.VITE_BASE_URL;
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const register = async (
   name: string,
